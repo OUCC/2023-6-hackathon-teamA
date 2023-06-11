99562c61fc3a0d70deead681785129279e4d046d
DESKTOP-SOAPNRO\nodah <tcen.being.85024@gmail.com>
2023-06-11 15:22:25 +0900

    BattleManagerスクリプトファイルが誕生

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    private BattleSystem battleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        battleSystem = new BattleSystem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void attackText()
+    {
+
+    }
+}

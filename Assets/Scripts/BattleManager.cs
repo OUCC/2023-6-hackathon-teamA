5e89c9d3d6d4e86047ced114888739ceb7e19e80
DESKTOP-SOAPNRO\nodah <tcen.being.85024@gmail.com>
2023-06-12 19:49:55 +0900

    テキスト部分を一通り作りました。他クラストの擦り合わせは今後行っていきます。

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,58 +1,92 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BattleManager : MonoBehaviour
+﻿namespace HackathonA
 {
-    const int attacking = 0;
-    const int magicAttacking = 1;
-    const int healing = 2;
-    const int counter = 3;
-    const int magicCounter = 4;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    /*
-     actionText
-     ・攻撃時、ダメージのテキストを返す。
-         */
-    string actionText(int action, int absDamage, string actor)
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class BattleManager : MonoBehaviour
     {
-        string message = "";
-        string target;
-        if (actor == "player")
+        const int attacking = 0;
+        const int magicAttacking = 1;
+        const int healing = 2;
+        const int counter = 3;
+        const int magicCounter = 4;
+        private BattleSystem battleSystem;
+        private BattleData battleData;
+        // Start is called before the first frame update
+        void Start()
         {
-            target = "enemy";
+            battleSystem = new BattleSystem();
         }
-        else
+
+        /*
+         actionText
+         ・攻撃時、ダメージのテキストを返す。
+             */
+        public string actionText(int action)
         {
-            target = "player";
+            battleData = battleSystem.getBattleData(action);
+            string message = "";
+            var charactorList = new List<string>();
+            if (battleData.actionJudge)
+            {
+                charactorList.Add("Player");
+                charactorList.Add("Enemy");
+            }
+            else
+            {
+                charactorList.Add("Enemy");
+                charactorList.Add("Player");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                string actor = charactorList[i];
+                string target = charactorList[i ^ 1];
+                switch (action)
+                {
+                    case attacking:
+                        message += actor + "の攻撃!\n";
+                        message += target + "は" + battleData.damageValue[actor] + "のダメージを受けた";
+                        break;
+                    case magicAttacking:
+                        message += actor + "の魔法攻撃!\n";
+                        message += target + "は" + battleData.damageValue[actor] + "のダメージを受けた";
+                        break;
+                    case healing:
+                        message += actor + "は" + battleData.damageValue[actor] + "回復した";
+                        break;
+                    case counter:
+                        if (battleData.counterJudge[actor])
+                        {
+                            message += actor + "のカウンター攻撃!\n";
+                            message += target + "は" + battleData.damageValue[actor] + "のダメージを受けた";
+                        }
+                        else
+                        {
+                            message += actor + "のカウンター攻撃!\n";
+                            message += "しかし失敗に終わった。";
+                        }
+                        break;
+                    case magicCounter:
+                        if (battleData.counterJudge[actor])
+                        {
+                            message += actor + "の魔法カウンター攻撃!\n";
+                            message += target + "は" + battleData.damageValue[actor] + "のダメージを受けた";
+                        }
+                        else
+                        {
+                            message += actor + "の魔法カウンター攻撃!\n";
+                            message += "しかし失敗に終わった。";
+                        }
+                        break;
+                }
+                if (i == 1)
+                {
+                    message += "\n";
+                }
+            }
+            return message;
         }
-        switch (action)
-        {
-            case attacking:
-                message += actor + "の攻撃!\n";
-                message += target + "は" + absDamage.ToString() + "のダメージを受けた";
-                break;
-            case magicAttacking:
-                message += actor + "の魔法攻撃!\n";
-                message += target + "は" + absDamage.ToString() + "のダメージを受けた";
-                break;
-            case healing:
-                message += actor + "は" + absDamage.ToString() + "回復した";
-                break;
-            case counter:
-                message += actor + "のカウンター攻撃!\n";
-                message += target + "は" + absDamage.ToString() + "のダメージを受けた";
-                break;
-            case magicCounter:
-                message += actor + "の魔法カウンター攻撃!\n";
-                message += target + "は" + absDamage.ToString() + "のダメージを受けた";
-                break;
-        }
-        return message;
     }
 }

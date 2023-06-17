8993a3f13bca754a37d8ada4922d250ab520364e
watanabe4869 <105962248+watanabe4869@users.noreply.github.com>
2023-06-17 16:37:13 +0900

    BattleDataにインターフェース追加。BattleSystemにMainメソッド追加。

--- Assets/Scripts/BattleData.cs
+++ Assets/Scripts/BattleData.cs
@@ -3,28 +3,71 @@ using System.Collections.Generic;
 
 namespace HackathonA
 {
+    interface ICharactar
+    {
+        int Hp { get; set; }
+        int ActionType { get; set; }
+        int DamageValue { get; set; }
+        bool CounterJudge { get; set; }
+    }
+
+    public class Player : ICharactar
+    {
+        public Player(int hp = 100, int actionType = 0, int damageValue = 0, bool counterJudge = false)
+        {
+            Hp = hp;
+            ActionType = actionType;
+            DamageValue = damageValue;
+            CounterJudge = counterJudge;
+        }
+
+        public int Hp
+        {
+            get => _hp;
+            set => _hp = value > 0 ? value : 0;
+        }
+        private int _hp;
+
+        public int ActionType { get; set; }
+        public int DamageValue { get; set; }
+        public bool CounterJudge { get; set; }
+
+    }
+    public class Enemy : ICharactar
+    {
+        public Enemy(int hp = 100, int actionType = 0, int damageValue = 0, bool counterJudge = false)
+        {
+            Hp = hp;
+            ActionType = actionType;
+            DamageValue = damageValue;
+            CounterJudge = counterJudge;
+        }
+
+        public int Hp
+        {
+            get => _hp;
+            set => _hp = value > 0 ? value : 0;
+        }
+        private int _hp;
+
+        public int ActionType { get; set; }
+        public int DamageValue { get; set; }
+        public bool CounterJudge { get; set; }
+
+    }
+
+
     public class BattleData
     {
-        //HP
-        public readonly int hp;
+        //勝敗判定（１：player勝利、０：継戦、ー１：enemy勝利）
+        public readonly int battleJudge;
+        //先行判定（true：player先行）
+        public readonly bool moveJudge;
 
-        //行動（０：物理攻撃、１：魔法攻撃、２：回復、３：物理カウンター、４：魔法カウンター）
-        public readonly int action;
-
-        //ダメージ
-        public readonly int damageValue;
-
-        //カウンター成功判定（成功：１、失敗：０）
-
-        public readonly int counterJudge;
-
-        public BattleData(int _hp,int _action,int _damageValue, int _counterJudge)
+        public BattleData(int battleJudge, bool moveJudge)
         {
-            this.hp = _hp;
-            this.action = _action;
-            this.damageValue = _damageValue;
-            this.counterJudge = _counterJudge;
-
+            this.battleJudge = battleJudge;
+            this.moveJudge = moveJudge;
         }
     }
 }
--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,101 +1,122 @@
 using System;
+
 namespace HackathonA
 {
 
     public class BattleSystem
     {
-        //カウンターの成功判定（成功が1、失敗が0）
-        private static (int playerCounterJudge, int enemyCounterJudge) CounterJudge(int playerAction, int enemyAction)
+        public void Main(int playerAction)
         {
-            int _playerAction = playerAction;
-            int _enemyAction = enemyAction;
-            int playerCounterJudge = 0;
-            int enemyCounterJudge = 0;
+            Player player = new Player();
+            Enemy enemy = new Enemy();
+
+            player.ActionType = playerAction;
+
+            //ChatGPTから取得
+            enemy.ActionType = 0;
+
+            (player.DamageValue, enemy.DamageValue, player.CounterJudge, enemy.CounterJudge) = Damage(player.ActionType, enemy.ActionType);
+
+            player.Hp = HpCalculate(player.Hp, player.ActionType, player.DamageValue);
+            enemy.Hp = HpCalculate(enemy.Hp, enemy.ActionType, enemy.DamageValue);
+
+
+            int battleJudge = BattleJudge(player.Hp,enemy.Hp);
+            bool moveJudge = MoveJudge(player.CounterJudge);
+            BattleData battle = new BattleData(battleJudge,moveJudge);
+        }
+
+
+        //カウンターの成功判定（成功が1、失敗が0）
+        private (bool playerCounterJudge, bool enemyCounterJudge) CounterJudge(int playerAction, int enemyAction)
+        {
+            bool playerCounterJudge = false;
+            bool enemyCounterJudge = false;
 
             //player側の判定
-            if (_playerAction == 3)
+            if (playerAction == 3)
             {
-                switch (_enemyAction)
+                switch (enemyAction)
                 {
                     case 0:
-                        playerCounterJudge = 1;
+                        playerCounterJudge = true;
                         break;
                     case 1:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 2:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 3:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 5:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                 }
             }
-            else if (_playerAction == 4)
+            else if (playerAction == 4)
             {
-                switch (_enemyAction)
+                switch (enemyAction)
                 {
                     case 0:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 1:
-                        playerCounterJudge = 1;
+                        playerCounterJudge = true;
                         break;
                     case 2:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 3:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                     case 5:
-                        playerCounterJudge = 0;
+                        playerCounterJudge = false;
                         break;
                 }
             }
 
             //enemy側の判定
-            else if (_enemyAction == 3)
+            else if (enemyAction == 3)
             {
-                switch (_playerAction)
+                switch (playerAction)
                 {
                     case 0:
-                        enemyCounterJudge = 1;
+                        enemyCounterJudge = true;
                         break;
                     case 1:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 2:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 3:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 5:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                 }
             }
-            else if (_enemyAction == 4)
+            else if (enemyAction == 4)
             {
-                switch (_playerAction)
+                switch (playerAction)
                 {
                     case 0:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 1:
-                        enemyCounterJudge = 1;
+                        enemyCounterJudge = true;
                         break;
                     case 2:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 3:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                     case 5:
-                        enemyCounterJudge = 0;
+                        enemyCounterJudge = false;
                         break;
                 }
             }
@@ -103,11 +124,11 @@ namespace HackathonA
         }
 
         //ダメージ計算
-        private static (int playerDamageValue, int enemyDamageValue) Damage(int playerAction, int enemyAction)
+        private (int playerDamageValue, int enemyDamageValue,bool playerCounterJudge,bool enemyCounterJudge) Damage(int playerAction, int enemyAction)
         {
             int playerDamageValue = 0;
             int enemyDamageValue = 0;
-            (int playerCounterJudge, int enemyCounterJudge) = BattleSystem.CounterJudge(playerAction, enemyAction);
+            (bool playerCounterJudge, bool enemyCounterJudge) = CounterJudge(playerAction, enemyAction);
 
             var rand = new Random();
             switch (playerAction)
@@ -139,32 +160,32 @@ namespace HackathonA
             //カウンターの処理
             switch (playerCounterJudge)
             {
-                case 0:
+                case true:
                     playerAction = 0;
                     break;
 
-                case 1:
+                case false:
                     playerDamageValue = enemyDamageValue;
                     enemyDamageValue = 0;
                     break;
             }
             switch (enemyCounterJudge)
             {
-                case 0:
+                case true:
                     enemyDamageValue = 0;
                     break;
 
-                case 1:
+                case false:
                     enemyDamageValue = playerDamageValue;
                     playerDamageValue = 0;
                     break;
             }
-            return (playerDamageValue, enemyDamageValue);
+            return (playerDamageValue, enemyDamageValue, playerCounterJudge, enemyCounterJudge);
         }
 
 
         //HP計算
-        private static int HpCalculate(int hp, int action, int damageValue)
+        private int HpCalculate(int hp, int action, int damageValue)
         {
             int _hp = hp;
             int _damageValue = damageValue;
@@ -183,7 +204,7 @@ namespace HackathonA
         }
 
         //勝敗判定（１：player勝利、０：継戦、ー１：enemy勝利）
-        private static int BattleJudge(int playerHp, int enemyHp)
+        private int BattleJudge(int playerHp, int enemyHp)
         {
             int _playerhp = playerHp;
             int _enemyhp = enemyHp;
@@ -204,6 +225,17 @@ namespace HackathonA
             }
 
             return judge;
+        }
+        private bool MoveJudge(bool counterJudge)
+        {
+            bool moveJudge = true;
+            if (counterJudge == true)
+            {
+                moveJudge = false;
+            }
+
+            return moveJudge;
+
         }
 
     }
